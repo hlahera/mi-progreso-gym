d9e19251051db34d41702f648de7bcb6bbb0cacc
harold <107411684+hlahera@users.noreply.github.com>
2025-06-12 10:24:20 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,7 @@ const ASSETS = [
     '/styles.css',
     '/app.js',
     '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png',
+  '/gg.jng',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
